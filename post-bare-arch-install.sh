93110ee1b34c9801971f58ebf18d73be04f7aa11
flakmoppen <flakmoppen@gmail.com>
2024-03-02 00:24:39 +0100

    Added arch install json file, and a bare arch install script to get sway.

---
+++ post-bare-arch-install.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+echo
+echo "INSTALLING SWAY AND PACKAGES."
+echo
+
+PACKAGES=(
+        "git",
+        "curl",
+        "wireplumber",
+        "pipewire-pulse",
+        "xorg-xwayland",
+        "wlroots",
+        "polkit",
+        "sway",
+        "swaybg",
+        "swayidle",
+        "grim",
+        "slurp",
+        "alacritty",
+        "waybar",
+        "wofi",
+        "ttf-nerd-fonts-symbols",
+        "ttf-hack-nerd",
+        "lsof",
+)
+
+for PACKAGE in "${PACKAGES[@]}"; do
+    echo "INSTALLING: ${PACKAGE}"
+    sudo pacman -S "$PACKAGE" --noconfirm --needed
+done
+
+echo
+echo "Done!"
+echo
