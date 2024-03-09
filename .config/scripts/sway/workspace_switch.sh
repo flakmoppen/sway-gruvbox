f377bdfec34a5cc1b84ae3c76cf8d29b52e7e385
flakmoppen <flakmoppen@gmail.com>
2024-03-09 23:20:02 +0100

    Finalized stylings in css

--- .config/scripts/sway/workspace_switch.sh
+++ .config/scripts/sway/workspace_switch.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
+#$mod+Control+Shift+1 [con_id=__focused__] move to workspace 1, focus
 
 current_workspace=$(swaymsg -p -t get_workspaces|grep focused|grep -o "[0-9]")
 
 if [ "$1" = "window" ]; then
-	category="swaymsg move window to workspace"
-	shift
+    category="swaymsg move window to workspace"
+    shift
 else
-	category="swaymsg workspace number"
+    category="swaymsg workspace number"
 fi
 
 if [ "$1" = "next" ]; then
-	test $current_workspace -eq 4 && exit 1
-	$category $((current_workspace + 1))
+    test $current_workspace -eq 4 && exit 1
+    $category $((current_workspace + 1))
 elif [ "$1" = "prev" ]; then
-	test $current_workspace -eq 1 && exit 1
-	$category $((current_workspace - 1))
+    test $current_workspace -eq 1 && exit 1
+    $category $((current_workspace - 1))
 fi
